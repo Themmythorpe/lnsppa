cd652b131498d2ed942678b963a46db69411078f
Themmythorpe <soge.tope@gmail.com>
2024-11-12 01:09:44 +0100

    update on cors

--- src/api/user.js
+++ src/api/user.js
@@ -2,7 +2,7 @@ import request from '@/utils/request'
 
 export function login(data) {
   return request({
-    url: '/login',
+    url: '/auth/login',
     method: 'post',
     data
   })
@@ -21,9 +21,6 @@ export function logout() {
     method: 'post'
   })
 }
-
-
-
 
 // import request from '@/utils/request'
 
